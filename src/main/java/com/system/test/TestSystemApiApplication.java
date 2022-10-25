5e2c5565c7b14bf75bb3db58f884de07b6c65cdc
busyohot <busyo@nate.com>
2022-10-25 21:35:21 +0900

    Initial commit

---
+++ src/main/java/com/system/test/TestSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.system.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/system/test/TestSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
